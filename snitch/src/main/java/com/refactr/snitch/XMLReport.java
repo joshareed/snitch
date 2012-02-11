ac47185e1c75f70b8b0a00ea0388949d9a2ee598
Josh Reed <josh@refactr.com>
2012-02-11 08:40:40 -0600

    Indent the XML report

--- snitch/src/main/java/com/refactr/snitch/XMLReport.java
+++ snitch/src/main/java/com/refactr/snitch/XMLReport.java
@@ -1,19 +1,32 @@
 package com.refactr.snitch;
 
 import java.io.IOException;
+import java.io.StringReader;
+import java.io.StringWriter;
 import java.io.Writer;
 
 import javax.xml.stream.FactoryConfigurationError;
 import javax.xml.stream.XMLOutputFactory;
 import javax.xml.stream.XMLStreamException;
 import javax.xml.stream.XMLStreamWriter;
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.Result;
+import javax.xml.transform.Source;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerConfigurationException;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.TransformerFactoryConfigurationError;
+import javax.xml.transform.stream.StreamResult;
+import javax.xml.transform.stream.StreamSource;
 
 public class XMLReport implements Report {
 
 	@Override
 	public void build(final SnitchResult results, final Writer writer) throws IOException {
 		try {
-			XMLStreamWriter xml = XMLOutputFactory.newFactory().createXMLStreamWriter(writer);
+			StringWriter out = new StringWriter();
+			XMLStreamWriter xml = XMLOutputFactory.newFactory().createXMLStreamWriter(out);
 			xml.writeStartDocument();
 			xml.writeStartElement("report");
 			xml.writeStartElement("project");
@@ -35,9 +48,23 @@ public class XMLReport implements Report {
 			xml.writeEndDocument();
 			xml.flush();
 			xml.close();
+
+			// format the report
+			Source source = new StreamSource(new StringReader(out.toString()));
+			Result result = new StreamResult(writer);
+			Transformer transformer = TransformerFactory.newInstance().newTransformer();
+			transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+			transformer.setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "4");
+			transformer.transform(source, result);
 		} catch (XMLStreamException e) {
 			throw new IOException("Error writing XML", e);
 		} catch (FactoryConfigurationError e) {
+			throw new IOException("Unable to get XML writer", e);
+		} catch (TransformerConfigurationException e) {
+			throw new IOException("Unable to get XML writer", e);
+		} catch (TransformerFactoryConfigurationError e) {
+			throw new IOException("Unable to get XML writer", e);
+		} catch (TransformerException e) {
 			throw new IOException("Unable to get XML writer", e);
 		}
 	}
