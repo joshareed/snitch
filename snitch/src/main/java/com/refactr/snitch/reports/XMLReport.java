a31a17c5ddfbdecb643c92e2a0c75b11f6c9f60a
Josh Reed <josh@refactr.com>
2012-02-15 21:30:54 -0600

    Tweak the XML report to remove redundancy

--- snitch/src/main/java/com/refactr/snitch/reports/XMLReport.java
+++ snitch/src/main/java/com/refactr/snitch/reports/XMLReport.java
@@ -4,7 +4,9 @@ import java.io.File;
 import java.io.IOException;
 import java.io.Writer;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import javax.xml.stream.FactoryConfigurationError;
 import javax.xml.stream.XMLOutputFactory;
@@ -33,11 +35,32 @@ public class XMLReport implements Report {
 			xml.writeEmptyElement("project");
 			xml.writeAttribute("name", results.getProject().getName());
 			xml.writeAttribute("path", results.getProject().getCanonicalPath());
-			xml.writeStartElement("violations");
+
+			xml.writeEmptyElement("summary");
+			xml.writeAttribute("files", "" + results.getFiles());
+			xml.writeAttribute("lines", "" + results.getLines());
+			xml.writeAttribute("violations", "" + results.getViolations().size());
+			xml.writeAttribute("time", "" + results.getTime());
+
 			List<Violation> violations = results.getViolations();
 			Collections.sort(violations);
+
+			// build our rule map
+			Map<String, String> rules = new HashMap<String, String>();
+			for (Violation v : violations) {
+				rules.put(v.getRule(), v.getMessage());
+			}
+			xml.writeStartElement("rules");
+			for (String rule : rules.keySet()) {
+				xml.writeEmptyElement("rule");
+				xml.writeAttribute("id", rule);
+				xml.writeAttribute("message", rules.get(rule));
+			}
+			xml.writeEndElement();
+
+			xml.writeStartElement("violations");
 			File current = null;
-			for (Violation v : results.getViolations()) {
+			for (Violation v : violations) {
 				if (current != v.getFile()) {
 					if (current != null) {
 						xml.writeEndElement();
@@ -54,7 +77,6 @@ public class XMLReport implements Report {
 				}
 				xml.writeAttribute("rule", v.getRule());
 				xml.writeAttribute("line", Integer.toString(v.getLine()));
-				xml.writeAttribute("message", v.getMessage());
 			}
 			xml.writeEndDocument();
 			xml.flush();
